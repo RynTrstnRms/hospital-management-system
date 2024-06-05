f3c8e9acae1299189b97e2ccc82de0406634323d
allysasamante <samanteallysa@gmail.com>
2024-06-05 15:41:35 -0700

    update api.php
    update api

--- hospi-backend/routes/api.php
+++ hospi-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +18,9 @@ use App\Http\Controllers\PatientController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::get('/patients', [PatientController::class, 'index'])->name('patients.index');
+Route::get('/patients/{id}', [PatientController::class, 'show'])->name('patients.show');
+Route::post('/patients', [PatientController::class, 'store'])->name('patients.store');
+Route::put('/patients/{id}', [PatientController::class, 'update'])->name('patients.update');
+Route::delete('/patients/{id}', [PatientController::class, 'destroy'])->name('patients.destroy');
