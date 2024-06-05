bb0dd3c65d875ad767470a05ba43dde702a409e5
Mhaerrick-Riego-de-Dios <riegodediosmhaerrick14@gmail.com>
2024-06-05 14:25:38 +0800

    User-Seeder-Commit

--- hospi-backend/app/Models/User.php
+++ hospi-backend/app/Models/User.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
 
     protected $fillable = [
         'name', 'email', 'password', 'role',
