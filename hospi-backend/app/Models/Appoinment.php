84d959370dabde8e0235d05c82fa7fc5de6e3357
crisryanRavello <ravellocrisryan81@gmail.com>
2024-06-05 13:57:24 +0800

    Update Appointment.php

--- hospi-backend/app/Models/Appoinment.php
+++ hospi-backend/app/Models/Appoinment.php
@@ -5,23 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Appointment extends Model
+class Appoinment extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'patient_id', 'doctor_id', 'appointment_date', 'remarks',
+        'patient_id', 'doctor_id', 'appointment_date', 'remarks', 
     ];
 
-    public function patient()
-    {
+    public function patient(){
         return $this->belongsTo(Patient::class);
     }
 
-    public function doctor()
-    {
+    public function doctor(){
         return $this->belongsTo(Doctor::class);
     }
+    
 }
-
-
