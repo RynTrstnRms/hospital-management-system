dda90a39fcaaf73cc36b6cbad5baf9e3134135e6
allysasamante <samanteallysa@gmail.com>
2024-06-05 15:01:24 -0700

    route
    create route

--- hospi-backend/routes/api.php
+++ hospi-backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PatientController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+oute::get('/patients', [PatientController::class, 'index'])->name('patients.index');
